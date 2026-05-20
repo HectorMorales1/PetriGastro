316898ded84bbe7b59cd9d12b2aaa1f3fcfd4e3d
hector.moralescampana@gmail.com <practicas03@CCB206>
2026-05-20 12:26:31 +0200

    refactor: remove migration and seeder scripts; clean up database initialization

--- server/database/migrations/002_add_destacado.sql
+++
@@ -1,5 +0,0 @@
--- Add destacado (featured) column to platos
-ALTER TABLE platos ADD COLUMN IF NOT EXISTS destacado BOOLEAN DEFAULT false;
-
--- Index for faster queries of featured dishes
-CREATE INDEX IF NOT EXISTS idx_platos_destacado ON platos(destacado) WHERE destacado = true;
--- server/database/migrations/003_add_apellidos.sql
+++
@@ -1,2 +0,0 @@
--- Add apellidos column to usuarios
-ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS apellidos VARCHAR(100);
